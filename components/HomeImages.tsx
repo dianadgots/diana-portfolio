6b3169c0b2dbe42742568a59fd060a040074ae5e
Diana Gots <dianagots@Dianas-MBP-2.lan>
2026-02-24 21:21:11 -0500

    Mobile homepage changes

--- components/HomeImages.tsx
+++ components/HomeImages.tsx
@@ -5,30 +5,35 @@ export default function HomeImages() {
         <div className="home-img-col">
           <div className="home-img-ration">
             <img
-              src="/imgs/public/imgs/1.png"
+              src="/imgs/public/imgs/HomeImages/1.png"
               alt="Diana Gots"
               className="home-img"
             />
           </div>
           <div className="home-img-ration">
             <img
-              src="/imgs/public/imgs/2.png"
+              src="/imgs/public/imgs/HomeImages/2.png"
               alt="Diana Gots"
               className="home-img"
             />
           </div>
         </div>
         <div className="home-img-col center">
-          <div className="home-img-ration">
+          <div className="home-img-ration home-img-slot-swap">
             <img
-              src="/imgs/public/imgs/3.png"
+              src="/imgs/public/imgs/HomeImages/3.png"
               alt="Diana Gots"
-              className="home-img"
+              className="home-img home-img-object-top home-img-desktop-only"
+            />
+            <img
+              src="/imgs/public/imgs/HomeImages/5.jpg"
+              alt="Diana Gots"
+              className="home-img home-img-mobile-only"
             />
           </div>
           <div className="home-img-ration">
             <img
-              src="/imgs/public/imgs/4.JPG"
+              src="/imgs/public/imgs/HomeImages/4.JPG"
               alt="Diana Gots"
               className="home-img"
             />
@@ -37,14 +42,14 @@ export default function HomeImages() {
         <div className="home-img-col">
           <div className="home-img-ration">
             <img
-              src="/imgs/public/imgs/5.jpg"
+              src="/imgs/public/imgs/HomeImages/5.jpg"
               alt="Diana Gots"
               className="home-img"
             />
           </div>
           <div className="home-img-ration">
             <img
-              src="/imgs/public/imgs/6.png"
+              src="/imgs/public/imgs/HomeImages/6.png"
               alt="Diana Gots"
               className="home-img"
             />
